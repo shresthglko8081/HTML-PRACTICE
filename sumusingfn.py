552f2417a9a90ced281bdd94661430b27f82d89b
Shresth Gupta <uday.shresthgupta@gmail.com>
2025-11-21 15:46:34 +0530

    Add files via upload

---
+++ sumusingfn.py
@@ -0,0 +1,14 @@
+s=int(input("Enter the value of first number : "))
+t=int(input("Enter the value of second number : "))
+def sum(s,t):
+    return (s+t)
+print("The sum of two numbers is : ",sum(s,t))
+def minus(s,t):
+    return (s-t)
+print("The difference of two numbers is : ",minus(s,t))
+def multiply(s,t):
+    return (s*t)
+print("The product of two numbers is : ",multiply(s,t))
+def divide(s,t):
+    return (s/t)
+print("The division of two numbers is : ",divide(s,t))The total output is:
